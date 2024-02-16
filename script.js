da54b9f5750e8af5a8656bd87d2d2f0e4dd6c02b
MulengaHankandaChituta <mulengahankandachituta@gmail.com>
2024-02-16 10:11:55 +0200

    Updated my code with a function that plays a single round of rock paper scissors

--- script.js
+++ script.js
@@ -8,5 +8,34 @@ function getComputerChoice() {
   return computerChoice;
 }
 
-// function being put to use
-console.log(getComputerChoice());
+// Function to play a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+  // Convert playerSelection to lowercase for case insensitivity
+  playerSelection = playerSelection.toLowerCase();
+
+  //  Define the winning scenarios
+  const winConditions = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
+  };
+
+  // The code below checkss if it's a tie
+  if (playerSelection === computerSelection) {
+    return "It's a tie";
+  }
+
+  // The code below checks if a player wins
+  if (winConditions[playerSelection] === computerSelection) {
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
+  }
+
+  // If not a tie and player doesn't win, computer wins
+  return `You Lose! ${computerSelection} beats ${playerSelection}`;
+}
+
+// Functions being put to use
+const playerChoice = "rock"; // This assumes a player's choice is rock
+const computerChoice = getComputerChoice(); // This gets computer's choice
+console.log("Computer's choice:", computerChoice);
+console.log(playRound(playerChoice, computerChoice));
